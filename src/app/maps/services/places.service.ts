fe8cf6e1ac864217a27674ba6adfd4f26e5fa8d8
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-07 15:17:37 +0200

    Realizar petición HTTP para obtener lugares

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,16 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PlacesResponse, Feature } from '../interfaces/places';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class PlacesService {
 	public useLocation!: [number, number];
+	public isLoadingPlaces: boolean = false;
+	public places: Feature[] = [];
 
 	get isUserLocationReady(): boolean {
 		return !!this.useLocation;
 	}
 
-	constructor() {
+	constructor(private http: HttpClient) {
 		this.getUserLocation();
 	}
 
@@ -28,5 +32,21 @@ export class PlacesService {
 				}
 			);
 		});
+	}
+
+	getPlacesByQuery(query: string = '') {
+		// TODO: evaluar cuando el query es nulo
+
+		this.isLoadingPlaces = true;
+
+		this.http
+			.get<PlacesResponse>(
+				`https://api.mapbox.com/search/geocode/v6/forward?q=${query}&limit=5&proximity=-5.843373156754296%2C43.35199690720651&language=es&access_token=pk.eyJ1Ijoiam9yZ2VwYXJlZGVzIiwiYSI6ImNsdjVpcXZtbjAzNTAya256N21jMTI4anoifQ.EYFEbi5LFINPVyC38vAJzw`
+			)
+			.subscribe((resp) => {
+				console.log(resp.features);
+				this.isLoadingPlaces = false;
+				this.places = resp.features;
+			});
 	}
 }
