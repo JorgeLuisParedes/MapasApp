f34cbb00e888b3128acff47f754a6ce862d4804f
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-07 11:03:08 +0200

    Mostrar un mapa de Mapbox

--- src/app/maps/components/maps-view/maps-view.component.ts
+++ src/app/maps/components/maps-view/maps-view.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { PlacesService } from '../../services';
+import { Map } from 'mapbox-gl';
 
 @Component({
 	selector: 'app-maps-view',
 	templateUrl: './maps-view.component.html',
 	styleUrl: './maps-view.component.css',
 })
-export class MapsViewComponent implements OnInit {
+export class MapsViewComponent implements AfterViewInit {
+	@ViewChild('mapDiv') mapDivElement!: ElementRef;
+
 	constructor(private placesSevices: PlacesService) {}
-	ngOnInit(): void {
-		console.log(this.placesSevices.useLocation);
+	ngAfterViewInit(): void {
+		if (!this.placesSevices) throw Error('No hay placesService.userLocation');
+		const map = new Map({
+			container: this.mapDivElement.nativeElement,
+			style: 'mapbox://styles/mapbox/light-v11',
+			center: this.placesSevices.useLocation,
+			zoom: 14,
+		});
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import Mapboxgl from 'mapbox-gl';
+Mapboxgl.accessToken = 'pk.eyJ1Ijoiam9yZ2VwYXJlZGVzIiwiYSI6ImNsdjVpcXZtbjAzNTAya256N21jMTI4anoifQ.EYFEbi5LFINPVyC38vAJzw';
+
 if (!navigator.geolocation) {
 	alert('Navegador no soporta la Geolocation');
 	throw new Error('Navegador no soporta la Geolocation');
