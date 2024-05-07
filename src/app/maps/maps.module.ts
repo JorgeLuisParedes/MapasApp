aef9929e92d048c3a40f3d91abe7f591b5c1d67f
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-07 13:07:23 +0200

    Diseño de componentes de búsqueda y detalles

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -5,9 +5,11 @@ import { MapsViewComponent } from './components/maps-view/maps-view.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { BtnMyLocationComponent } from './components/btn-my-location/btn-my-location.component';
 import { AngularLogoComponent } from './components/angular-logo/angular-logo.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
 
 @NgModule({
-	declarations: [MapsScreenComponent, MapsViewComponent, LoadingComponent, BtnMyLocationComponent, AngularLogoComponent],
+	declarations: [MapsScreenComponent, MapsViewComponent, LoadingComponent, BtnMyLocationComponent, AngularLogoComponent, SearchBarComponent, SearchResultsComponent],
 	imports: [CommonModule],
 	exports: [MapsScreenComponent],
 })
