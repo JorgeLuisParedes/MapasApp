3c36c11e1c8592e9e0b9a9193f91d1d5b28196c9
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-07 14:10:55 +0200

    Debounce Manual

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+	selector: 'app-search-bar',
+	templateUrl: './search-bar.component.html',
+	styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
+	private debounceTimer?: NodeJS.Timeout;
+	constructor() {}
 
+	onQueryChanged(query: string = '') {
+		if (this.debounceTimer) clearTimeout(this.debounceTimer);
+
+		this.debounceTimer = setTimeout(() => {
+			console.log('Mandar este query:', query);
+		}, 350);
+	}
 }
