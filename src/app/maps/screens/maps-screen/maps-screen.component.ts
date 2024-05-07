b54f856bf8e0c7770c45be1ba05a0a8fb2fb28c4
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-07 10:17:15 +0200

    Obtener la geolocalización del usuario

--- src/app/maps/screens/maps-screen/maps-screen.component.ts
+++ src/app/maps/screens/maps-screen/maps-screen.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
-  selector: 'app-maps-screen',
-  templateUrl: './maps-screen.component.html',
-  styleUrl: './maps-screen.component.css'
+	selector: 'app-maps-screen',
+	templateUrl: './maps-screen.component.html',
+	styleUrl: './maps-screen.component.css',
 })
-export class MapsScreenComponent {
-
+export class MapsScreenComponent implements OnInit {
+	constructor(private placeService: PlacesService) {}
+	ngOnInit(): void {}
 }
